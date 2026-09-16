04ff5ea8cad43147033cb767ef0f5bc32df175f6
Aaron gil <aaronabdiel09@gmai.com>
2026-09-16 02:58:48 -0400

    +

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,39 +2,41 @@
     import  Icon  from  '@iconify/svelte' ;
 </script>
 
-<section class="flex flex-col items-stretch text-mauve-200  px-3 py-2 font-bold">
+<section class="flex flex-col items-stretch text-mauve-200   px-3 py-2 font-bold">
     <p class="text-mist-200">SELECT YOUR SYSTEM</p>
     <p class="text-mist-400">Access guides of games and optimize your gaming experience.maximun performance</p> 
     <p class="py-4">AYUDAS RAPIDAS</p>
 
-    <div class="flex justify-between bg-mauve-950">
+    <div class="flex justify-between ">
         
-        <a href="bios-setup" class="flex items-center gap-1.5">
+        <a href="bios-setup" class="flex items-center gap-1.5 rounded-full text-cyan-300 bg-mauve-900 border-4  border-b-cyan-300 border-r-4 border-b-4  border-l-4 border-t-4 px-1 py-1 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
             <Icon icon="heroicons-solid:chip" />
-            <p class="text-cyan-300">Bios setup</p>
+            <p class="">Bios setup</p>
         </a>
 
-        <a href="controller-mapping" class="flex items-center gap-1.5">
+        <a href="controller-mapping" class="flex items-center gap-1 rounded-full bg-mauve-900-300 border-4 text-cyan-300 border-b-cyan-300 border-r-4 border-b-4  border-l-4 border-t-4 px-1 py-1 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
            <Icon icon="teenyicons:nes-outline" />
-            <p class="text-cyan-300">controller mapping</p>
+            <p class="">controller mapping</p>
         </a>
 
-        <a href="optimization-guide" class="flex items-center gap-1.5"> 
+        <a href="optimization-guide" class="flex items-center gap-1.5 rounded-full bg-mauve-900  border-4 text-cyan-300 border-b-cyan-300 border-r-4 border-b-4  border-l-4 border-t-4 px-1 py-1 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"> 
        <Icon icon="eos-icons:compass" />
-        <p class= "text-cyan-300 ">optimization guide</p>
+        <p class= " ">optimization guide</p>
         </a>
     </div>
 
-      <section class="flex flex-col justify-betweens text-mauve-200 font-bold">
+    <section class="flex flex-col justify-betweens text-mauve-200 font-bold">
 
         <div>
-        <p class=" flex-3 text-mist-200 20">Featured games</p>
+        <p class=" flex-3 text-mist-200">Featured games</p>
+        </div>
 
         <div class="flex text-center justify-between px-1 py-40 relative pt-40 pb-10">
         <p>Forza Horizon</p>
         <img src="/forza.jpg" class="absolute top-0 left-0 -z-10 w-small h-full object-cover " alt="">
-         
-       <div class="pt-6 pr-80">
+        
+
+        <div class="pt-6 pr-80">
         <p class="object-top-left">Resident Evil:Requiem</p>
         <img src="/resident.jpg" class="absolute top-0 center-10 -z-10 w-small h-full object-cover  object-center"  alt="">
         </div>
@@ -43,6 +45,7 @@
             <p>Mario Kart</p>
             <img src="/mario.jpg" class="absolute top-0 right-0 -z-10 w-small h-full object-cover " alt="">
         </div>
+
     </section>
 
     <div class="pt-10">
@@ -51,11 +54,9 @@
     
     
     
-    <section class="flex  justify-between items-center  px-50 py-2 ">
+    <section class="flex  justify-between items-center  px-50 py-2 gap-13">
 
-    
-
-        <div class="pt-5  gap-1.5">
+        <div class="pt-5  gap-1.5 rounded-xl bg-mauve-900  border-4 text-cyan-300 border-b-cyan-300 border-r-4 border-b-4  border-l-4 border-t-4 px-5 py-5">
             <p class="text-cyan-300">shadow of the colossus</p>
             <p class=" text-xs text-balance">La historia sigue a Wander,
                  un joven que viaja hacia una desolada
@@ -64,14 +65,14 @@
             <button class="">READ GUIDE</button>
         </div>
 
-        <div class="">
+        <div class="rounded-xl bg-mauve-900  border-4 text-cyan-300 border-b-cyan-300 border-r-4 border-b-4  border-l-4 border-t-4 px-5 py-5">
             <p class="text-cyan-300">No Man's Sky</p>
             <p class="text-xs text-balance">No Man's Sky es un juego de ciencia ficción sobre exploración 
             y supervivencia en un universo infinito generado de forma procedimental.</p>
             <button>READ GUIDE</button>
         </div>
 
-        <div>
+        <div class="rounded-xl bg-mauve-900  border-4 text-cyan-300 border-b-cyan-300 border-r-4 border-b-4  border-l-4 border-t-4 px-5 py-5">
             <p class="text-cyan-300">Big Walk</p>
             <p class="text-xs text-balance">Pasa el rato con tus amigos y perdeos en un mundo enorme.
                  Una caminata cooperativa en línea de los creadores de Untitled Goose Game.</p>
@@ -101,7 +102,7 @@
         </div>
 
         <div>
-            <p class="text-cyan-300">PC</p>
+            <p class="text-cyan-300"  >PC</p>
             <p>Tarjetas gráficas: Las GPUs de Nvidia y AMD mantienen una tendencia de aumento en costos por el encarecimiento de la memoria GDDR6 y GDDR7</p>
         </div>
     </section>
--- src/routes/xbox/+page.svelte
+++ src/routes/xbox/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section class="flex justify-end items-center gap-2 py-50 text-mauve-200">
-    <div class="flex-1  relative pt-70 pb-10  px-47 border-4 border-text-cyan-400text rounded-3xl overflow-hidden">
+    <div class="flex-1  relative pt-110 pb-50  px-47 border-4 border-text-cyan-400text rounded-3xl overflow-hidden">
       
         <p class="font-bold text-3xl">HALO:COMBAT ENVOLVED</p>
        <img src="/halo.jpg" class="absolute top-0 left-0 -z-10 w-full h-full object-cover" alt="">
